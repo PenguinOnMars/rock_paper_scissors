227ad04f74b8fd0069d52ef98f745a7c154c06fd
Gigo <arielrsam@gmail.com>
2022-12-08 16:01:54 -0500

    Correct script src

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // If you select a block of code and use the key sequence Ctrl+K+C, 
 // you'll comment out the section of code. Ctrl+K+U will uncomment the code.
-//    function getRandomInt(3) {
-//     return Math.floor(Math.random() * 3);
-//   }
-//   if (getRandomInt == 0) {
-//       console.log("Rock");
+//function getRandomInt() {
+//  return Math.floor(Math.random() * 3);
+//}
+//if (getRandomInt == 0) {
+//  console.log("Rock");
 //   }
 //   else if (getRandomInt == 1) {
 //       console.log("Paper");
@@ -12,4 +12,3 @@
 //   else {
 //       console.log("Scissors");
 //   }
-  console.log("Hello World")
