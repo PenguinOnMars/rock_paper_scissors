ec2349bcded0ad68c294311c9ac5f2270cd10501
Gigo <arielrsam@gmail.com>
2022-12-08 15:47:56 -0500

    Add javascipt src

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// If you select a block of code and use the key sequence Ctrl+K+C, 
+// you'll comment out the section of code. Ctrl+K+U will uncomment the code.
+//    function getRandomInt(3) {
+//     return Math.floor(Math.random() * 3);
+//   }
+//   if (getRandomInt == 0) {
+//       console.log("Rock");
+//   }
+//   else if (getRandomInt == 1) {
+//       console.log("Paper");
+//   }
+//   else {
+//       console.log("Scissors");
+//   }
+  console.log("Hello World")
