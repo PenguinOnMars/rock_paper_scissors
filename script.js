398882475c91b8be3b6e35a989fad0444fed7c23
Gigo <arielrsam@gmail.com>
2022-12-08 17:57:29 -0500

    Update computerSelection & playerPrompt

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 // If you select a block of code and use the key sequence Ctrl+K+C, 
 // you'll comment out the section of code. Ctrl+K+U will uncomment the code.
-//function getRandomInt() {
-//  return Math.floor(Math.random() * 3);
-//}
-//if (getRandomInt == 0) {
-//  console.log("Rock");
-//   }
-//   else if (getRandomInt == 1) {
-//       console.log("Paper");
-//   }
-//   else {
-//       console.log("Scissors");
-//   }
+function getComputerChoice(){
+  return Math.floor(Math.random() * 3);
+} 
+
+let computerSelection = getComputerChoice(3);
+console.log(computerSelection);
+
+if(computerSelection === 0){
+  computerSelection = "rock";
+}
+else if(computerSelection === 1){
+  computerSelection = "paper";
+}
+else{
+  computerSelection = "scissors";
+}
+console.log(computerSelection);
+
+// let playerSelection = getUserChoice();
+
+let playerPrompt = prompt("rock, paper, or scissors?").toLowerCase();
+console.log(playerPrompt);
+
+let playerSelection = 0;
+
+function getUserChoice(){
+  if (playerPrompt === "rock"){
+    playerSelection = "rock";
+    return playerSelection;
+  } 
+  else if (playerSelection === "paper"){
+    playerSelection = "paper";
+    return playerSelection;
+  }
+  else if (playerSelection === "scissors"){
+    playerSelection = "scissors";
+    return playerSelection;
+  }
+  else {
+  }
+}
+
+getUserChoice();
+
+console.log(playerSelection);
+
+function whoWins(){
+
+}
