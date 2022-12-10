ad9c25a5fbf9d3bc4c32d44a543f0440024bc2d1
Gigo <arielrsam@gmail.com>
2022-12-10 14:34:18 -0500

    Fix winner and loop

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // If you select a block of code and use the key sequence Ctrl+K+C, 
 // you'll comment out the section of code. Ctrl+K+U will uncomment the code.
+let winner;
+let user;
+let computer;
+let noOne;
+
 function getComputerChoice(){
   return Math.floor(Math.random() * 3);
 } 
@@ -20,92 +25,110 @@ console.log(computerSelection);
 
 let playerSelection;
 
-while ((playerSelection != "rock") && (playerSelection != "paper") && (playerSelection != "scissors")){
-  playerSelection = prompt("rock, paper, or scissors?").toLowerCase();
-  console.log(playerSelection);
-  
-  compare(playerSelection, computerSelection);
+for (let i = 0; i < 5; i++){
+  game(i);
 }
 
-function compare(playerSelection, computerSelection){
-  if (playerSelection == "scissors"){
-    if (computerSelection == "rock"){
-      console.log("computer wins :(");
+function game(){
+    while ((playerSelection != "rock") && (playerSelection != "paper") && (playerSelection != "scissors")){
+      playerSelection = prompt("rock, paper, or scissors?").toLowerCase();
+      console.log(playerSelection);
+      playRound(playerSelection, computerSelection);
     }
-    else if (computerSelection == "paper"){
-      console.log("you win! :)");
-    }
-    else {
-      console.log("it's a tie");
-    }
-  }
-  else if (playerSelection == "rock"){
-    if (computerSelection == "paper"){
-      console.log("computer wins :(");
-    }
-    else if(computerSelection == "scissors"){
-      console.log("you win! :)");
-    }
-    else {
-      console.log("it's a tie");
-    }
-  }
-  else if (playerSelection == "paper"){
-    if (computerSelection == "rock"){
-      console.log("you win! :)");
-    }
-    else if(computerSelection == "scissors"){
-      console.log("computer wins :(");
-    }
-    else {
-      console.log("it's a tie");
-    }
-  }
 }
 
-/* compare user vs computer
-  if paper vs rock 
-  paper wins
-  if rock vs scissors
-  rock win
-  if scissors vs rock
-  scissors wins
-  else its a tie
-
-
-
-
-
-
-
-// let playerSelection = getUserChoice();
-
-//let playerPrompt = prompt("rock, paper, or scissors?").toLowerCase();
-//console.log(playerPrompt);
-
-//let playerSelection = 0;
-
-/*function getUserChoice(){
-  if (playerPrompt === "rock"){
-    playerSelection = "rock";
-    return playerSelection;
-  } 
+function playRound(playerSelection, computerSelection){
+  if (playerSelection === "scissors"){
+    if (computerSelection === "rock"){
+      return winner = computer;
+    }
+    else if (computerSelection === "paper"){
+      return winner = user;
+    }
+  }
+  else if (playerSelection === "rock"){
+    if (computerSelection === "paper"){
+      return winner = computer;
+    }
+    else if(computerSelection === "scissors"){
+      return winner = user;
+    }
+  }
   else if (playerSelection === "paper"){
-    playerSelection = "paper";
-    return playerSelection;
-  }
-  else if (playerSelection === "scissors"){
-    playerSelection = "scissors";
-    return playerSelection; 
+    if (computerSelection === "rock"){
+      return winner = user;
+    }
+    else if(computerSelection === "scissors"){
+      return winner = computer;
+    }
   }
   else {
+    return winner = noOne;
+  }
+  pickWinner();
+}
+
+function pickWinner(){
+  if (computer == winner){
+    return("computer wins :(");
+  }
+  else if (user == winner){
+    return("you win! :)");
+  }
+  else {
+    return("it's a tie");
   }
 }
-*/
-//getUserChoice();
+console.log(pickWinner());
+//console.log(playRound(playerSelection, computerSelection));
+// /* compare user vs computer
+//   if paper vs rock 
+//   paper wins
+//   if rock vs scissors
+//   rock win
+//   if scissors vs rock
+//   scissors wins
+//   else its a tie
 
-//console.log(playerSelection);
 
-//function whoWins(){
 
-//}
+
+
+//     //playRound(playerSelection, computerSelection);
+
+
+
+// function playRound(playerSelection, computerSelection){
+//   if (playerSelection == "scissors"){
+//     if (computerSelection == "rock"){
+//       return("computer wins :(");
+//     }
+//     else if (computerSelection == "paper"){
+//       return("you win! :)");
+//     }
+//     else {
+//       return("it's a tie");
+//     }
+//   }
+//   else if (playerSelection == "rock"){
+//     if (computerSelection == "paper"){
+//       return("computer wins :(");
+//     }
+//     else if(computerSelection == "scissors"){
+//       return("you win! :)");
+//     }
+//     else {
+//       return("it's a tie");
+//     }
+//   }
+//   else if (playerSelection == "paper"){
+//     if (computerSelection == "rock"){
+//       return("you win! :)");
+//     }
+//     else if(computerSelection == "scissors"){
+//       return("computer wins :(");
+//     }
+//     else {
+//       return("it's a tie");
+//     }
+//   }
