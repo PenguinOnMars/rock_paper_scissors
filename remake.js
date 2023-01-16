b147989094bdc0ba00ff24439f32633b5d10559f
Gigo <arielrsam@gmail.com>
2023-01-16 15:30:45 -0500

    Make counter work, up to 5!

--- remake.js
+++ remake.js
@@ -7,8 +7,10 @@
 //record winner
 //prompt user again, record winner 1st to 5 wins
 
-let userWinCount = 0;
-let computerWinCount = 0;
+// const userScore = document.getElementById('data-user-score');
+// const computerScore = document.getElementById('data-computer-score');
+let uScore = 0;
+let cScore = 0;
 
 //for (i = 0; i < 5; i++){
 function getComputerChoice(){
@@ -29,72 +31,109 @@ function getComputerChoice(){
     }
 }
 
-let computerSelection = getComputerChoice();
 //console.log(computerSelection);
 
-const rock = document.createElement('button');
-rock.innerText = 'rock';
 
-const paper = document.createElement('button');
-paper.innerText = 'paper';
+let playerSelection;
+let computerSelection;
+let winner;
 
-const scissors = document.createElement('button');
-paper.innerText = 'scissors';
+const selectionButtons = document.querySelectorAll('[data-selection]')
 
-function getUserChoice(){
-    
-}    
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        
+        playerSelection = selectionButton.dataset.selection;
+        computerSelection = getComputerChoice();
+        winner = playRound(playerSelection, computerSelection);
 
-let playerSelection = getUserChoice();
+        console.log(playerSelection);
+        console.log(computerSelection);
+        console.log(winner);
+
+        if (winner == "user") parseInt(userScore) + 1;
+        if (winner == "computer") parseInt(computerScore) + 1;
+    })
+    //console.log(playerSelection);
+})
 //console.log(playerSelection);
+// function makeSelection(selection) {
+//     console.log(selection);
+// }
 
-function playRound(){
+function playRound(playerSelection, computerSelection){
     if (playerSelection === "rock"){
         if (computerSelection === "paper"){
-            alert("computer wins :(");
+            //alert("computer wins :(");
+            cScore++;
+            updateScore();
             return "computer";
         }
         else if (computerSelection === "rock"){
-            alert("it's a tie");
+            //alert("it's a tie");
             return "tie";
         }
-        alert("you win! :D");
+        //alert("you win! :D");
+        uScore++;
+        updateScore();
         return "user";
     }
     else if (playerSelection === "paper"){
         if (computerSelection === "scissors"){
-            alert("computer wins :(");
+            //alert("computer wins :(");
+            cScore++;
+            updateScore();
             return "computer";
         }
         else if (computerSelection === "paper"){
-            alert("it's a tie");
+            //alert("it's a tie");
             return "tie";
         }
-        alert("you win! :D");
+        //alert("you win! :D");
+        uScore++;
+        updateScore();
         return "user";
     }
     else if (playerSelection === "scissors"){
         if (computerSelection === "rock"){
-            alert("computer wins :(");
+            //alert("computer wins :(");
+            cScore++;
+            updateScore();
             return "computer";
         }
         else if (computerSelection === "scissors"){
-            alert("it's a tie");
+            //alert("it's a tie");
             return "tie";
         }
-        alert("you win! :D");
+        //alert("you win! :D");
+        uScore++;
+        updateScore();
         return "user";
     }
 }
 
-let winner = playRound();
-if (winner == "user"){
-    userWinCount++;
-}
-else if (winner == "computer"){
-    computerWinCount++;
+const updateScore = () => {
+    const userScore = document.getElementById('userScore');
+    const computerScore = document.getElementById('computerScore');
+    const bigWinner = document.getElementById('bigWinner');
+    userScore.textContent = uScore;
+    computerScore.textContent = cScore;
+    if (cScore == 5) {
+        bigWinner.textContent = 'computer wins :('
+    }
+    else if (uScore == 5) {
+        bigWinner.textContent = 'you win! :D'
+    }
 }
 
+
+
+// const updateScore = () => {
+//     // let userScore = document.querySelector('userScore');
+//     // let computerScore = document.querySelector('computerScore');
+//     userScore.textContent = uScore;
+//     computerScore.textContent = cScore;
+// };
 
 
 //let computerSelection = getComputerChoice();
@@ -103,12 +142,12 @@ else if (winner == "computer"){
 //console.log(playerSelection);
 
 
-alert("player score: " + userWinCount + "\ncomputer score: " + computerWinCount);
-//}
-if (userWinCount > computerWinCount){
-    alert("YOU WIN!");
-}
-else if (userWinCount < computerWinCount){
-    alert("YOU LOSE!");
-}
-else alert("IT'S A TIE");
+// alert("player score: " + userWinCount + "\ncomputer score: " + computerWinCount);
+// //}
+// if (userWinCount > computerWinCount){
+//     alert("YOU WIN!");
+// }
+// else if (userWinCount < computerWinCount){
+//     alert("YOU LOSE!");
+// }
+// else alert("IT'S A TIE");
